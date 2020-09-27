d5ed65bdc4a15960aa7501487496e66619b1e170
Silbert <Firmino>
2020-09-26 22:57:15 -0300

    Setando o desconto

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Desafio Ecommerce
 
+Desenvolver um projeto para extrair um catálogo de produtos de um E-Commerce.
+
 Desafio realizado utilizando o site da amazon para consultar produtos.
 
 # Tecnologias
@@ -11,7 +13,9 @@ Desafio realizado utilizando o site da amazon para consultar produtos.
 
 # Execução
 
-Foi criado uma api REST para consultar os produtos cadastrados.
+Basta executar a classe DesafioEcommerceApplication.java para executar o projeto.
+
+Foi criado uma api REST para consultar os produtos cadastrados. Utilizei a ferramenta insomnia para executar os endpoints.
 
 Foram criados 5 endpoints, são eles:
 
@@ -31,3 +35,5 @@ Foram criados 5 endpoints, são eles:
 5- Lista SOMENTE os produtos que apresenta respectivamente: Menor preço, mais popular e com maior desconto:
 > http://localhost:8080/api/produtos
 
+
+Com o banco de dados criado basta executar primeiro o endpoint **http://localhost:8080/api/salvarProdutos** para o JSoup capturar os produtos e salvar no banco.
--- src/main/java/com/desafio/service/JsoupService.java
+++ src/main/java/com/desafio/service/JsoupService.java
@@ -36,8 +36,13 @@ public class JsoupService {
 			String titulo = element.getElementsByClass("p13n-sc-truncate-mobile-type").text();
 			String avaliacao = element.getElementsByClass("a-size-small a-link-normal").text();
 			String preco = element.getElementsByClass("p13n-sc-price").text();
-			String desconto = "";
-			String link = "";
+			
+			Element link = element.select("span > a.a-link-normal[href]").first();
+			String href = link.attr("href");
+			String urlProduto = "https://www.amazon.com.br"+href;
+			
+			Document paginaLivroIndividual = Jsoup.connect(urlProduto).get();
+			Element precoAntigo = paginaLivroIndividual.select("div#buyBoxInner > ul >li > span > span.a-color-secondary.a-text-strike").first();
 			
 			produto.setNome(titulo);
 			if(!avaliacao.trim().equals("")) {
@@ -46,9 +51,16 @@ public class JsoupService {
 				produto.setClassificacao(0);
 			}
 			produto.setPreco(new BigDecimal(preco.replaceAll("[^0-9]+", "")));
-			BigDecimal valorDesconto = BigDecimal.ZERO;
-			produto.setDesconto(valorDesconto);
-			produto.setURL("https://www.amazon.com.br/gp/bestsellers/books");
+			produto.setURL(urlProduto);
+
+			if(precoAntigo != null) {
+				BigDecimal precoAnterior = new BigDecimal(precoAntigo.text().replaceAll("[^0-9]+", ""));
+				BigDecimal desconto = precoAnterior.subtract(produto.getPreco());
+				produto.setDesconto(desconto);
+			}else {
+				BigDecimal valorDesconto = BigDecimal.ZERO;
+				produto.setDesconto(valorDesconto);
+			}
 			
 			produtos.add(produto);
 		}
@@ -59,7 +71,46 @@ public class JsoupService {
 	
 	public static void main(String[] args) throws IOException {
 		
+		List<Produto> produtos = new ArrayList<>();
 		
-		
+		Document pagina = Jsoup.connect("https://www.amazon.com.br/gp/bestsellers/books").get();
+
+		Elements ol = pagina.select("ol.a-ordered-list.a-vertical");
+		Elements li = ol.select("li.zg-item-immersion");
+
+		for (Element element : li) {
+			Produto produto = new Produto();
+			
+			String titulo = element.getElementsByClass("p13n-sc-truncate-mobile-type").text();
+			String avaliacao = element.getElementsByClass("a-size-small a-link-normal").text();
+			String preco = element.getElementsByClass("p13n-sc-price").text();
+			
+			Element link = element.select("span > a.a-link-normal[href]").first();
+			String href = link.attr("href");
+			String urlProduto = "https://www.amazon.com.br"+href;
+			
+			Document paginaLivroIndividual = Jsoup.connect(urlProduto).get();
+			Element precoAntigo = paginaLivroIndividual.select("div#buyBoxInner > ul >li > span > span.a-color-secondary.a-text-strike").first();
+			
+			produto.setNome(titulo);
+			if(!avaliacao.trim().equals("")) {
+				produto.setClassificacao(Long.parseLong(avaliacao.replaceAll("[^0-9]+", "")));
+			}else {
+				produto.setClassificacao(0);
+			}
+			produto.setPreco(new BigDecimal(preco.replaceAll("[^0-9]+", "")));
+			produto.setURL(urlProduto);
+
+			if(precoAntigo != null) {
+				BigDecimal precoAnterior = new BigDecimal(precoAntigo.text().replaceAll("[^0-9]+", ""));
+				BigDecimal desconto = precoAnterior.subtract(produto.getPreco());
+				produto.setDesconto(desconto);
+			}else {
+				BigDecimal valorDesconto = BigDecimal.ZERO;
+				produto.setDesconto(valorDesconto);
+			}
+			
+			produtos.add(produto);
+		}
 	}
 }
